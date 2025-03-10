4cedf9bbd6133f205bb01508f44b57c98b610a3f
Luca Balasoiu <balasoiu2005@yahoo.com>
2025-03-11 01:03:16 +0200

    Animatia reparata

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,53 @@ import  { useState } from 'react';
 import { motion , AnimatePresence  } from 'framer-motion';
 
 const App = () => {
-  const [showCards, setShowCards] = useState(true);
+  const [activeSection, setActiveSection] = useState('');
   return (
     <div className=" flex flex-row bg-gray-400">
     <div class="flex flex-col h-screen w-25 bg-gray-700 items-center p-5 ">
-      <button className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><SearchIcon/></button>
-      <button className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><UserIcon/></button>
-      <button className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><SettingsIcon/></button>
-      <button onClick={() => setShowCards(!showCards)} className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><CircleHelpIcon/></button>
+      <button onClick={() => setActiveSection('money')} className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><SearchIcon/></button>
+      <button onClick={() => setActiveSection('user')} className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><UserIcon/></button>
+      <button onClick={() => setActiveSection('settings')} className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><SettingsIcon/></button>
+      <button onClick={() => setActiveSection('help')} className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><CircleHelpIcon/></button>
       <button className="flex align-baseline items-center justify-center p-4 bg-gray-400 m-240 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><LogOutIcon/></button>
 
     </div>
-    <AnimatePresence>
-      {showCards && (
-      <motion.div
-      className="grid bg-gray-400 h-screen w-full items-center justify-center grid-cols-4 place-items-center"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: showCards ? 1 : 0 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 1 }}>
-        
-        <div className="flex flex-col justify-center w-100 h-130 bg-blue-300 rounded-2xl shadow-cyan-950/50 shadow-lg hover:cursor-pointer transition duration-400 hover:shadow-cyan-950 hover:ring-2 text-cyan-950/50">
+    <div className="flex justify-center items-center h-screen w-full bg-gray-400">
+      <AnimatePresence>
+        {activeSection === 'money' && (
+          <motion.div
+          key="money"
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.9 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          layout={false}
+          className="flex justify-center items-center h-screen w-full"
+          >
+            <div>Subscriptions</div>
+          </motion.div>
+        )}
+        {activeSection === 'help' && (
+          <motion.div
+          key="help"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          style={{
+            position: "absolute", 
+            top: "50%", 
+            left: "50%", 
+            transform: "translate(-50%, -50%)", // Centrează pe orizontală și verticală
+            display: "grid",
+            gridTemplateColumns: "repeat(4, 1fr)", // Configurează coloanele
+            gridGap: "160px", // Adaugă spațiu între elemente
+            justifyItems: "center", // Centrează elementele pe orizontală în grid
+            alignItems: "center", // Centrează elementele pe verticală în grid
+          }}          
+          className="grid grid-cols-2 md:grid-cols-4 grid-rows-2 gap-40"
+          >
+          <div className="flex flex-col justify-center w-100 h-130 bg-blue-300 rounded-2xl shadow-cyan-950/50 shadow-lg hover:cursor-pointer transition duration-400 hover:shadow-cyan-950 hover:ring-2 text-cyan-950/50">
           <div className="flex items-center justify-center h-80 w-100 ">
             <div className="flex items-center justify-center bg-white h-70 w-70 rounded-full ring-6">
              <img src="https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?auto=compress&cs=tinysrgb&w=600" className="h-70 w-70 rounded-full"></img>
@@ -127,10 +153,17 @@ const App = () => {
             <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 m-4 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Hire</div>
           </div>
         </div>
+        </motion.div>
+
+        )}
+      </AnimatePresence>
+      
         
-      </motion.div>
-      )}
-    </AnimatePresence>
+        
+        
+      
+    
+    </div>
    </div>
     
   )
