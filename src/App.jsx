6af3e4915e56d060e524c891323f13331ae73d73
Luca Balasoiu <balasoiu2005@yahoo.com>
2025-03-11 20:12:05 +0200

    Inceputul design ului pentru cardurile de subscription

--- src/App.jsx
+++ src/App.jsx
@@ -41,14 +41,44 @@ const App = () => {
           className="flex justify-center items-center h-screen w-full"
           >
             <div className="flex flex-row items-center justify-center">
-              <div className="flex items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:cursor-pointer hover:ring-2 hover:text-cyan-600">
-
+              <div className="flex flex-col items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:ring-3 hover:text-cyan-600">
+                <div className="flex items-center justify-center w-130 h-30 rounded-t-3xl">
+                  Titlu
+                </div>
+                <div className="flex items-center justify-center w-120 h-140 ">
+                  beneficii
+                </div>
+                <div className="flex items-center justify-center w-130 h-40 rounded-b-3xl">
+                  <button className="h-20 w-100 rounded-2xl text-white bg-cyan-600 hover:cursor-pointer text-2xl hover:bg-cyan-700 transition duration-300">
+                    Select
+                  </button>
+                </div>
               </div>
-              <div className="flex items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:cursor-pointer hover:ring-2 hover:text-cyan-600">
-
+              <div className="flex flex-col items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:cursor-pointer hover:ring-3 hover:text-cyan-600">
+               <div className="flex items-center justify-center w-130 h-30 rounded-t-3xl">
+                  Titlu
+                </div>
+                <div className="flex items-center justify-center w-120 h-140 ">
+                  beneficii
+                </div>
+                <div className="flex items-center justify-center w-130 h-40 rounded-b-3xl">
+                  <button className="h-20 w-100 rounded-2xl text-white bg-cyan-600 hover:cursor-pointer text-2xl hover:bg-cyan-700 transition duration-300">
+                    Select
+                  </button>
+                </div>
               </div>
-              <div className="flex items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:cursor-pointer hover:ring-2 hover:text-cyan-600">
-                
+              <div className="flex flex-col items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:cursor-pointer hover:ring-3 hover:text-cyan-600">
+                <div className="flex items-center justify-center w-130 h-30 rounded-t-3xl">
+                  Titlu
+                </div>
+                <div className="flex items-center justify-center w-120 h-140 ">
+                  beneficii
+                </div>
+                <div className="flex items-center justify-center w-130 h-40 rounded-b-3xl">
+                  <button className="h-20 w-100 rounded-2xl text-white bg-cyan-600 hover:cursor-pointer text-2xl hover:bg-cyan-700 transition duration-300">
+                    Select
+                  </button>
+                </div>
               </div>
             </div>
           </motion.div>
@@ -66,7 +96,7 @@ const App = () => {
             left: "12%", 
             transform: "translate(-50%, -50%)", // Centrează pe orizontală și verticală
             display: "grid",
-            gridTemplateColumns: "repeat(4, 1fr)", // Configurează coloanele
+            gridTemplateColumns: "repeat(4, 1fr)", 
             gridGap: "160px", // Adaugă spațiu între elemente
             justifyItems: "center", // Centrează elementele pe orizontală în grid
             alignItems: "center", // Centrează elementele pe verticală în grid
