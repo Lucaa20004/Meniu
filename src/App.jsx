2821bb5637ea2b886b34bed7c414099423f41247
Luca Balasoiu <balasoiu2005@yahoo.com>
2025-03-13 12:39:56 +0200

    A fost introdus react-rout si s au creat fisiere pentru fiecare route in parte

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import React from 'react'
 import { Button ,Tooltip } from "flowbite-react";
 import  { useState } from 'react';
 import { motion , AnimatePresence  } from 'framer-motion';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Help from "./components/Help";
+import Profile from "./components/Profile";
+import Settings from "./components/Settings";
+import Subscription from "./components/Subscription";
 
 const App = () => {
   const [activeSection, setActiveSection] = useState('');
   return (
+    <Router>
+    
+    <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/subscription" element={<Subscription />} />
+          <Route path="/help" element={<Help />} />
+    </Routes>
     <div className=" flex flex-row bg-gray-400">
     <div class="flex flex-col h-screen w-25 bg-gray-700 items-center p-5 ">
+
       <Tooltip content="Money" placement="right"  className="p-4 flex items-center justify-center" style="light">
-              <button onClick={() => setActiveSection('money')} className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><WalletIcon/></button>
+              <Link to="/subscription" className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><WalletIcon/></Link>
       </Tooltip>
 
       <Tooltip content="Profile" placement="right"  className="p-4 flex items-center justify-center" style="light">
@@ -22,10 +37,10 @@ const App = () => {
       </Tooltip>
 
       <Tooltip content="Help" placement="right"  className="p-4 flex items-center justify-center" style="light">
-              <button onClick={() => setActiveSection('help')} className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><CircleHelpIcon/></button>
+              <Link  to="/help"  className="flex items-center justify-center p-4 bg-gray-400 m-5 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><CircleHelpIcon/></Link>
       </Tooltip>
       
-      <button className="flex align-baseline items-center justify-center p-4 bg-gray-400 m-240 rounded-3xl hover:rounded-2xl tranistion duration-400 hover:cursor-pointer ring-2 text-white hover:bg-gray-600"><LogOutIcon/></button>
+      
 
     </div>
     <div className="flex justify-center items-center h-screen w-full bg-gray-400">
@@ -36,7 +51,7 @@ const App = () => {
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0.9 }}
-          transition={{ duration: 0.5, ease: "easeInOut" }}
+          transition={{ duration: 0.5, ease: "easeInOut"}}
           layout={false}
           className="flex justify-center items-center h-screen w-full"
           >
@@ -83,142 +98,11 @@ const App = () => {
             </div>
           </motion.div>
         )}
-        {activeSection === 'help' && (
-          <motion.div
-          key="help"
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.9 }}
-          transition={{ duration: 0.5 , ease: "easeInOut"}}
-          style={{
-            position: "absolute", 
-            top: "10%", 
-            left: "12%", 
-            transform: "translate(-50%, -50%)", // Centrează pe orizontală și verticală
-            display: "grid",
-            gridTemplateColumns: "repeat(4, 1fr)", 
-            gridGap: "160px", // Adaugă spațiu între elemente
-            justifyItems: "center", // Centrează elementele pe orizontală în grid
-            alignItems: "center", // Centrează elementele pe verticală în grid
-          }}          
-           
-          >
-          <div className="flex flex-col justify-center w-100 h-130 bg-blue-300 rounded-2xl shadow-cyan-950/50 shadow-lg hover:cursor-pointer transition duration-400 hover:shadow-cyan-950 hover:ring-2 text-cyan-950/50">
-          <div className="flex items-center justify-center h-80 w-100 ">
-            <div className="flex items-center justify-center bg-white h-70 w-70 rounded-full ring-6">
-             <img src="https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?auto=compress&cs=tinysrgb&w=600" className="h-70 w-70 rounded-full"></img>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-center h-50 w-100 bg-gray-500 rounded-b-2xl">
-            <div className="flex items-center justify-center text-white h-13 w-100 rounded-2xl text-2xl">Diana</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">  Contact</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 m-4 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Hire</div>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-center w-100 h-130 bg-orange-300 rounded-2xl shadow-cyan-950/50 shadow-lg hover:cursor-pointer transition duration-400 hover:shadow-cyan-950 hover:ring-2 text-cyan-950/50">
-         <div className="flex items-center justify-center h-80 w-100 ">
-            <div className="flex items-center justify-center bg-white h-70 w-70 rounded-full ring-6">
-             <img src="https://images.pexels.com/photos/36843/lion-panthera-leo-lioness-animal-world.jpg?auto=compress&cs=tinysrgb&w=600" className="h-70 w-70 rounded-full"></img>
-           </div>
-         </div>
-          <div className="flex flex-col items-center justify-center h-50 w-100 bg-gray-500 rounded-b-2xl">
-            <div className="flex items-center justify-center text-white h-13 w-100 rounded-2xl text-2xl">Tudor</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Contact</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 m-4 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Hire</div>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-center w-100 h-130 bg-pink-300 rounded-2xl shadow-cyan-950/50 shadow-lg hover:cursor-pointer transition duration-400 hover:shadow-cyan-950 hover:ring-2 text-cyan-950/50">
-          <div className="flex items-center justify-center h-80 w-100 ">
-            <div className="flex items-center justify-center bg-white h-70 w-70 rounded-full ring-6">
-              <img src="https://images.pexels.com/photos/18167822/pexels-photo-18167822/free-photo-of-close-up-of-a-cat.jpeg?auto=compress&cs=tinysrgb&w=600" className="h-70 w-70 rounded-full"></img>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-center h-50 w-100 bg-gray-500 rounded-b-2xl">
-            <div className="flex items-center justify-center text-white h-13 w-100 rounded-2xl text-2xl">Buruian</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Contact</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 m-4 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Hire</div>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-center w-100 h-130 bg-green-300 rounded-2xl shadow-cyan-950/50 shadow-lg hover:cursor-pointer transition duration-400 hover:shadow-cyan-950 hover:ring-2 text-cyan-950/50">
-          <div className="flex items-center justify-center h-80 w-100 ">
-            <div className="flex items-center justify-center bg-white h-70 w-70 rounded-full ring-6">
-              <img src="https://images.pexels.com/photos/67552/giraffe-tall-mammal-africa-67552.jpeg?auto=compress&cs=tinysrgb&w=600" className="h-70 w-70 rounded-full"></img>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-center h-50 w-100 bg-gray-500 rounded-b-2xl">
-            <div className="flex items-center justify-center text-white h-13 w-100 rounded-2xl text-2xl">Luca</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Contact</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 m-4 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Hire</div>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-center w-100 h-130 bg-purple-300 rounded-2xl shadow-cyan-950/50 shadow-lg hover:cursor-pointer transition duration-400 hover:shadow-cyan-950 hover:ring-2 text-cyan-950/50">
-          <div className="flex items-center justify-center h-80 w-100 ">
-            <div className="flex items-center justify-center bg-white h-70 w-70 rounded-full ring-6">
-              <img src="https://images.pexels.com/photos/236622/pexels-photo-236622.jpeg?auto=compress&cs=tinysrgb&w=600" className="h-70 w-70 rounded-full"></img>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-center h-50 w-100 bg-gray-500 rounded-b-2xl">
-            <div className="flex items-center justify-center text-white h-13 w-100 rounded-2xl text-2xl">Alexia</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Contact</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 m-4 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Hire</div>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-center w-100 h-130 bg-amber-300 rounded-2xl shadow-cyan-950/50 shadow-lg hover:cursor-pointer transition duration-400 hover:shadow-cyan-950 hover:ring-2 text-cyan-950/50">
-          <div className="flex items-center justify-center h-80 w-100 ">
-            <div className="flex items-center justify-center bg-white h-70 w-70 rounded-full ring-6">
-              <img src="https://images.pexels.com/photos/86405/penguin-funny-blue-water-86405.jpeg?auto=compress&cs=tinysrgb&w=600" className="h-70 w-70 rounded-full"></img>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-center h-50 w-100 bg-gray-500 rounded-b-2xl">
-            <div className="flex items-center justify-center text-white h-13 w-100 rounded-2xl text-2xl">Biris</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Contact</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 m-4 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Hire</div>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-center w-100 h-130 bg-red-300 rounded-2xl shadow-cyan-950/50 shadow-lg hover:cursor-pointer transition duration-400 hover:shadow-cyan-950 hover:ring-2 text-cyan-950/50">
-          <div className="flex items-center justify-center h-80 w-100 ">
-            <div className="flex items-center justify-center bg-white h-70 w-70 rounded-full ring-6">
-              <img src="https://images.pexels.com/photos/31027922/pexels-photo-31027922/free-photo-of-captivating-close-up-of-burrowing-owl-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600" className="h-70 w-70 rounded-full"></img>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-center h-50 w-100 bg-gray-500 rounded-b-2xl">
-            <div className="flex items-center justify-center text-white h-13 w-100 rounded-2xl text-2xl">Billy</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Contact</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 m-4 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Hire</div>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center justify-center w-100 h-130 bg-lime-300 rounded-2xl shadow-cyan-950/50 shadow-lg hover:cursor-pointer transition duration-400 hover:shadow-cyan-950 hover:ring-2 text-cyan-950/50">
-          <div className="flex items-center justify-center h-80 w-100 ">
-            <div className="flex items-center justify-center bg-white h-70 w-70 rounded-full ring-6">
-              <img src="https://images.pexels.com/photos/106686/pexels-photo-106686.jpeg?auto=compress&cs=tinysrgb&w=600" className="h-70 w-70 rounded-full"></img>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-center h-50 w-100 bg-gray-500 rounded-b-2xl">
-            <div className="flex items-center justify-center text-white h-13 w-100 rounded-2xl text-2xl">Matei</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Contact</div>
-            <div className="flex items-center justify-center text-black bg-gray-300 h-13 w-95 m-4 rounded-2xl text-2xl hover:text-cyan-600 hover:ring-2 hover:bg-gray-700 transition duration-400">Hire</div>
-          </div>
-        </div>
-        </motion.div>
-
-        )}
+      
       </AnimatePresence>
-      
-        
-        
-        
-      
-    
     </div>
    </div>
-    
+   </Router>
   )
 }
 
---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+const Contact = () => {
+    return (
+
+
+      <div className="flex justify-center items-center h-screen w-full bg-gray-400">
+        
+      </div>
+
+    ) 
+  };
+  
+  export default Contact;
