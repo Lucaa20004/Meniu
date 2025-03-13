b173d21e10192531bb0a6e1fe311163987901b76
Luca Balasoiu <balasoiu2005@yahoo.com>
2025-03-13 12:48:57 +0200

    s a creat ruta pentru subscription si pentru help

--- src/App.jsx
+++ src/App.jsx
@@ -44,62 +44,7 @@ const App = () => {
 
     </div>
     <div className="flex justify-center items-center h-screen w-full bg-gray-400">
-      <AnimatePresence>
-        {activeSection === 'money' && (
-          <motion.div
-          key="money"
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.9 }}
-          transition={{ duration: 0.5, ease: "easeInOut"}}
-          layout={false}
-          className="flex justify-center items-center h-screen w-full"
-          >
-            <div className="flex flex-row items-center justify-center">
-              <div className="flex flex-col items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:ring-3 hover:text-cyan-600">
-                <div className="flex items-center justify-center w-130 h-30 rounded-t-3xl">
-                  Titlu
-                </div>
-                <div className="flex items-center justify-center w-120 h-140 ">
-                  beneficii
-                </div>
-                <div className="flex items-center justify-center w-130 h-40 rounded-b-3xl">
-                  <button className="h-20 w-100 rounded-2xl text-white bg-cyan-600 hover:cursor-pointer text-2xl hover:bg-cyan-700 transition duration-300">
-                    Select
-                  </button>
-                </div>
-              </div>
-              <div className="flex flex-col items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:cursor-pointer hover:ring-3 hover:text-cyan-600">
-               <div className="flex items-center justify-center w-130 h-30 rounded-t-3xl">
-                  Titlu
-                </div>
-                <div className="flex items-center justify-center w-120 h-140 ">
-                  beneficii
-                </div>
-                <div className="flex items-center justify-center w-130 h-40 rounded-b-3xl">
-                  <button className="h-20 w-100 rounded-2xl text-white bg-cyan-600 hover:cursor-pointer text-2xl hover:bg-cyan-700 transition duration-300">
-                    Select
-                  </button>
-                </div>
-              </div>
-              <div className="flex flex-col items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:cursor-pointer hover:ring-3 hover:text-cyan-600">
-                <div className="flex items-center justify-center w-130 h-30 rounded-t-3xl">
-                  Titlu
-                </div>
-                <div className="flex items-center justify-center w-120 h-140 ">
-                  beneficii
-                </div>
-                <div className="flex items-center justify-center w-130 h-40 rounded-b-3xl">
-                  <button className="h-20 w-100 rounded-2xl text-white bg-cyan-600 hover:cursor-pointer text-2xl hover:bg-cyan-700 transition duration-300">
-                    Select
-                  </button>
-                </div>
-              </div>
-            </div>
-          </motion.div>
-        )}
       
-      </AnimatePresence>
     </div>
    </div>
    </Router>
--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,5 +1,59 @@
-const Contact = () => {
-    return <h1>Contact</h1>;
+const Subscription = () => {
+    return (
+
+      <div className="flex justify-center items-center h-screen w-full bg-gray-400">
+        <div className="flex flex-row items-center justify-center">
+              
+              <div className="flex flex-col items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:ring-3 hover:text-cyan-600">
+                <div className="flex items-center justify-center w-130 h-30 rounded-t-3xl">
+                  Titlu
+                </div>
+                <div className="flex items-center justify-center w-120 h-140 ">
+                  beneficii
+                </div>
+                <div className="flex items-center justify-center w-130 h-40 rounded-b-3xl">
+                  <button className="h-20 w-100 rounded-2xl text-white bg-cyan-600 hover:cursor-pointer text-2xl hover:bg-cyan-700 transition duration-300">
+                    Select
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:cursor-pointer hover:ring-3 hover:text-cyan-600">
+               <div className="flex items-center justify-center w-130 h-30 rounded-t-3xl">
+                  Titlu
+                </div>
+                <div className="flex items-center justify-center w-120 h-140 ">
+                  beneficii
+                </div>
+                <div className="flex items-center justify-center w-130 h-40 rounded-b-3xl">
+                  <button className="h-20 w-100 rounded-2xl text-white bg-cyan-600 hover:cursor-pointer text-2xl hover:bg-cyan-700 transition duration-300">
+                    Select
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center justify-center h-200 w-130 bg-white m-7 rounded-3xl hover:bg-gray-800 transition duration-400 ease-in-out hover:scale-110 hover:cursor-pointer hover:ring-3 hover:text-cyan-600">
+                <div className="flex items-center justify-center w-130 h-30 rounded-t-3xl">
+                  Titlu
+                </div>
+                <div className="flex items-center justify-center w-120 h-140 ">
+                  beneficii
+                </div>
+                <div className="flex items-center justify-center w-130 h-40 rounded-b-3xl">
+                  <button className="h-20 w-100 rounded-2xl text-white bg-cyan-600 hover:cursor-pointer text-2xl hover:bg-cyan-700 transition duration-300">
+                    Select
+                  </button>
+                </div>
+              </div>
+
+            </div>
+      </div>
+
+
+
+
+
+    )
   };
   
-  export default Contact;
+  export default Subscription;
